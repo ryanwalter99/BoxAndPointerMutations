660a31183657cf43955ca86dc89bde575a750530
Ryan Walter <walterrm@rose-hulman.edu>
2017-12-12 19:55:29 -0500

    Working

--- src/m3_implementing_functions_that_mutate.py
+++ src/m3_implementing_functions_that_mutate.py
@@ -3,8 +3,8 @@ This module lets you practice IMPLEMENTING
 functions that MUTATE their arguments.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Ryan Walter.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -145,7 +145,7 @@ def swap_colors(circle, rectangle):
     #      B = temp
     ####################################################################
     # ------------------------------------------------------------------
-
+    
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
